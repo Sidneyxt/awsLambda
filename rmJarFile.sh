83c9ff60b60f9bb179e7e80716fc26f67f622fe7
Sidney <xiaotian.chen@quvideo.com>
2017-12-15 11:34:17 +0800

    update

--- rmJarFile.sh
+++ rmJarFile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-7z d /Users/chenxiaotian/myWorkspace/awsLambda/target/aws-lambda-1.0-SNAPSHOT.jar org/bytedeco/javacpp/properties/android*
-7z d /Users/chenxiaotian/myWorkspace/awsLambda/target/aws-lambda-1.0-SNAPSHOT.jar aws-lambda-1.0-SNAPSHOT.jar org/bytedeco/javacpp/windows-x86_64
-7z d /Users/chenxiaotian/myWorkspace/awsLambda/target/aws-lambda-1.0-SNAPSHOT.jar aws-lambda-1.0-SNAPSHOT.jar org/bytedeco/javacpp/macosx-x86_64
+7z d target/aws-lambda-1.0-SNAPSHOT.jar org/bytedeco/javacpp/properties/android*
+7z d target/aws-lambda-1.0-SNAPSHOT.jar aws-lambda-1.0-SNAPSHOT.jar org/bytedeco/javacpp/windows-x86_64
+7z d target/aws-lambda-1.0-SNAPSHOT.jar aws-lambda-1.0-SNAPSHOT.jar org/bytedeco/javacpp/macosx-x86_64
